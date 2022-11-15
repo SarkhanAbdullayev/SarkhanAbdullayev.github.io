d5076584190543d5379980659b936354dd4034c6
Sarkhan <syako44@gmail.com>
2022-11-15 11:47:12 +0400

    Update app.js

--- weather/app.js
+++ weather/app.js
@@ -11,26 +11,29 @@ let humidity = document.querySelector(".HumidityP");
 let wind = document.querySelector(".WindP");
 let pressure = document.querySelector(".PressureP")
 
-let key = 'f54c305213296de227c8786a5bcf81cd'
+navigator.geolocation.getCurrentPosition((res) => {
 
-let weather = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=40.4226048&lon=49.8991104&appid=f54c305213296de227c8786a5bcf81cd`);
+    let key = 'f54c305213296de227c8786a5bcf81cd'
+    let lat = res.coords.latitude;
+    let long = res.coords.longitude;
 
+    let weather = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`);
 
-weather.then(res => {
-    return res.json()
-}).then(post => {
-    console.log(post);
-    let tarix = new Date(post.dt);
-    // const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    temph1.innerHTML = `${Math.round(post.main.temp - 273.15)}&deg`;
-    city.innerHTML = `${post.name.replace(" City", "")}`;
-    time.innerHTML = `${tarix.toGMTString()}`;
-    img.src = `http://openweathermap.org/img/wn/${post.weather[0].icon}@2x.png`
-    descriptP.innerHTML = `${post.weather[0].main}`;
+    weather.then(res => {
+        return res.json()
+    }).then(post => {
 
-    visibility.innerHTML = `${post.visibility/ 1000}km`
-    wind.innerHTML = `${post.wind.speed}m/s`;
-    humidity.innerHTML = `${post.main.humidity}%`
-    pressure.innerHTML = `${post.main.pressure}hPa`
-    // console.log(post.main.humidity);
-});
+        let currentData = new Date(post.dt);
+
+        temph1.innerHTML = `${Math.round(post.main.temp - 273.15)}&deg`;
+        city.innerHTML = `${post.name.replace(" City", "")}`;
+        time.innerHTML = `${currentData.toGMTString()}`;
+        img.src = `http://openweathermap.org/img/wn/${post.weather[0].icon}@2x.png`
+        descriptP.innerHTML = `${post.weather[0].main}`;
+    
+        visibility.innerHTML = `${post.visibility/ 1000}km`
+        wind.innerHTML = `${post.wind.speed}m/s`;
+        humidity.innerHTML = `${post.main.humidity}%`
+        pressure.innerHTML = `${post.main.pressure}hPa`
+    });
+})
